1bce88435dcf6d84a19549ce8d4fd6416b1632e2
Simon <simon.j.goring@gmail.com>
2013-09-10 20:55:53 -0500

    Changed title in README, added checks to save time when running so that if a data object is created (compile.pollen for example) that is large and takes a long time to build it can be saved in the `data/` folder and simply re-loaded rather than re-run.
    Deleted orphaned `load_datasets.R` file in parent directory.  It's not needed anymore.

--- R/load_datasets.R
+++ R/load_datasets.R
@@ -1,36 +1,56 @@
+library(neotoma)
 #  Okay, this is the code for the non-analogue analysis:
 
-all_sites <- get_datasets(datasettype = 'pollen')
-data.ids <- sapply(all_sites, function(x)x$DatasetID)
+#  Getting all the sites from neotoma is time consuming:
+if(!'all.sites.RData' %in% list.files('data/')){
+  all.datasets <- get_datasets(datasettype = 'pollen')
+  data.ids <- sapply(all.datasets, function(x)x$DatasetID)
 
-#  Return all XXX sites.
-all.downloads <- get_download(data.ids)
+  #  Return all pollen sites in neotoma:
+  all.sites <- get_download(data.ids)
+  save(all.sites, file='data/all.sites/RData')
+}
+if('all.sites.RData' %in% list.files('data/')){
+  load('data/all_sites.RData')
+}
 
-compiled.sites <- lapply(all.downloads, function(x) compile_list(x, list.name='WS64'))
+#  Compress the dataset taxonomies to the standard of the small Whitmore taxonomy.
+if('compiled.sites.RData' %in% list.files('data/')){
+  load('data/compiled.sites.RData')
+}
+if(!'compiled.sites.RData' %in% list.files('data/')){
+  compiled.sites <- lapply(all.sites, function(x) compile_list(x, list.name='WhitmoreSmall'))
+  save(compiled.sites, file='../data/compiled.sites.Rdata')  
+}
 
-save(compiled.sites, file='../data/compiled.sites.Rdata')
+#  Make the dataset into one giant table with site name, lat/long, depth and age and then counts:
+if(!'compiled.pollen.RData' %in% list.files('data/')){
+  for(i in i:length(compiled.sites)){
+    x <- compiled.sites[[i]]
+    
+    if(is.null(x$metadata$site.data$SiteName)) x$metadata$site.data$SiteName <- paste('NoName_ID', i)
+    if(is.null(x$sample.meta$depths)) x$sample.meta$depths <- NA
+    if(is.null(x$sample.meta$Age)) x$sample.meta$Age <- NA
+    
+    
+    site.info <- data.frame(sitename = x$metadata$site.data$SiteName,
+                            depth = x$sample.meta$depths,
+                            age = x$sample.meta$Age)
+    
+    if(i == 1){
+      compiled.pollen <- data.frame(site.info, x$counts)
+    } 
+    
+      if(i > 1) {
+        aa <- try(merge(compiled.pollen, data.frame(site.info, x$counts), all=TRUE))
+        if(length(aa) > 1) compiled.pollen <- aa
+      }
+    
+    cat(i, '\n')
+  }
+  save(compiled.pollen, file='compiled.pollen.RData')
+}
 
-for(i in i:length(compiled.sites)){
-  x <- compiled.sites[[i]]
-  
-  if(is.null(x$metadata$site.data$SiteName)) x$metadata$site.data$SiteName <- paste('NoName_ID', i)
-  if(is.null(x$sample.meta$depths)) x$sample.meta$depths <- NA
-  if(is.null(x$sample.meta$Age)) x$sample.meta$Age <- NA
-  
-  
-  site.info <- data.frame(sitename = x$metadata$site.data$SiteName,
-                          depth = x$sample.meta$depths,
-                          age = x$sample.meta$Age)
-  
-  if(i == 1){
-    compiled.pollen <- data.frame(site.info, x$counts)
-  } 
-  
-    if(i > 1) {
-      aa <- try(merge(compiled.pollen, data.frame(site.info, x$counts), all=TRUE))
-      if(length(aa) > 1) compiled.pollen <- aa
-    }
-  
-  #  wHY DO SOME SITES HAVE MORE METADATA THAN COUNTS?
-  cat(i, '\n')
+if('compiled.pollen.RData' %in% list.files('data/')){
+  load('data/compiled.pollen.RData')
 }
