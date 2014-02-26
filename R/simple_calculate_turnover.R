8c36747047073788b5ee6442dc4692806b04d587
Simon <simon.j.goring@gmail.com>
2014-02-25 22:10:36 -0600

    I still have to fix references in the Rmd file.  The new file `self.long` represents a sampling outline that Andria and I talked about over the internet chat. This provides all the dissimilarity values for samples within two time windows, 7500 - 8500 and 9500 - 10500 to help begin figuring out how a Bayesian model might be fit here.
    Other changes reflect the new structure of the `calculate_turnover` file, and represent the new run of the landscape dissimilarities.  This is where issues with the main Rmd file come up.  At this point it will not compile.

--- R/simple_calculate_turnover.R
+++ R/simple_calculate_turnover.R
@@ -1,8 +1,9 @@
-#  Calculate the dissimilarities for all sites in the large compiled.pollen data.frame.
-#  We want to calculate several metrics in two directions, forward and backwards,
-#  and we ideally want to keep track of where the closest landscape points are.
-#  Landscape similarity gives us a sense of how close a site is to an ecosystem that
-#  has previously existed on the landscape.  We also look forward to see
+#  This file is based on the calculate_turnover file, but is structured differently so that
+#  we see every sample that sits within the window.  This means a moderately larger data.frame for
+#  the self samples, and a significantly larger data.fframe for the landscape samples.
+#  However, this is simply to understand what the data actually look like, so we're limiting our
+#  analysis to the period from 9500 - 10500 14C years and from 7500 - 8500 14C years.
+#  Periods of high and low turnover respectively.
 
 library(snowfall)
 sfInit(parallel = TRUE, cpus = 4)
@@ -31,23 +32,33 @@ compiled.pollen$uID <- 1:nrow(compiled.pollen)
 #  the lapply functions require a lot of memory to keep everything together before
 #  dumping it out.
 
-if('self.frame.RData' %in% list.files('data')){
-  load('data/self.frame.RData')
-}
-else {
-  self.frame <- data.frame(site      = rep(compiled.pollen$sitename,2),
-                           dataset   = rep(compiled.pollen$dataset, 2),
-                           age       = rep(compiled.pollen$age, 2),
-                           index     = rep(1:nrow(compiled.pollen), 2),
-                           self.min  = rep(NA, nrow(compiled.pollen)*2),
-                           self.size = rep(NA, nrow(compiled.pollen)*2),
-                           delta.age = rep(NA, nrow(compiled.pollen)*2),
-                           mean.delt = rep(NA, nrow(compiled.pollen)*2),
-                           direction = rep(c('past', 'futu'), nrow(compiled.pollen)))
-  
-  for(i in 1:nrow(self.frame)){
+load('data/self.frame.RData')
+load('data/land.frame.RData')
+
+self.size <- sum(self.frame$self.size[findInterval(self.frame$age, c(7500, 8500, 9500, 10500)) %in% c(2, 4)], na.rm=TRUE)
+land.size <- sum(land.frame$sample.size[findInterval(land.frame$age, c(7500, 8500, 9500, 10500)) %in% c(2, 4)], na.rm=TRUE)
+
+
+#  This isn't the best way to do it yet.
+self.long <- data.frame(uID  =rep(NA, self.size), 
+                        aID  =rep(NA, self.size), 
+                        diss =rep(NA, self.size), 
+                        direction=rep(NA, self.size))
+
+land.long <- data.frame(uID  =rep(NA, land.size), 
+                        aID  =rep(NA, land.size), 
+                        diss =rep(NA, land.size), 
+                        direction=rep(NA, land.size))
+
+counter <- 1
+
+for(i in 1:nrow(self.frame)){
+  if(findInterval(self.frame$age[i], c(7500, 8500, 9500, 10500)) %in% c(2, 4)){
+    
     right.site <- compiled.pollen$sitename == self.frame$site[i]
     site.ages <-  compiled.pollen$age[right.site]
+    sample.age <- (compiled.pollen$age == self.frame$age[i])
+
     mult <- ifelse(self.frame$direction[i] == 'past',  1, -1)
     
     right.age <- findInterval(compiled.pollen$age,
@@ -62,15 +73,19 @@ else {
       calib.minus <- apply(calib.samples, 1, function(x) (arrow.mat - x)^2)
       calib.vals <- sqrt(colSums(calib.minus, na.rm=TRUE))
       
-      self.frame$self.min[i]  <- min(calib.vals)
-      self.frame$delta.age[i] <- compiled.pollen$age[right.age & right.site][which.min(calib.vals)] - compiled.pollen$age[i]
-      self.frame$self.size[i] <- length(calib.vals)
-      self.frame$mean.delt[i] <- mean(compiled.pollen$age[right.age & right.site] - compiled.pollen$age[i], na.rm=TRUE)
+      test <- data.frame(uID  = compiled.pollen$uID[floor((i+1)/2)],
+                         aID  = compiled.pollen$uID[right.site & right.age],
+                         diss = calib.vals,
+                         direction = as.character(self.frame$direction[i]),
+                         stringsAsFactors = FALSE)
+      
+      self.long[counter:(counter + length(calib.vals) - 1),] <- test
+      counter <- counter + length(calib.vals)
     }
   }
-  
-  save(self.frame, file = 'data/self.frame.RData')
 }
+
+save(self.long, file = 'data/self.long.RData')
 
 #  Now this is for the landscape metric.  I expect it to be slower:
 if('land.frame.RData' %in% list.files('data')){
