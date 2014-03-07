85d548cac4193ca93212aa6edf598172fc6df774
Simon <simon.j.goring@gmail.com>
2014-03-06 22:29:40 -0600

    Wrote the code to output the long table for landscapes as well now.  So we have both landscape and core data in long and 'strictly min' formats.

--- R/simple_calculate_turnover.R
+++ R/simple_calculate_turnover.R
@@ -38,17 +38,11 @@ load('data/land.frame.RData')
 self.size <- sum(self.frame$self.size[findInterval(self.frame$age, c(7500, 8500, 9500, 10500)) %in% c(2, 4)], na.rm=TRUE)
 land.size <- sum(land.frame$sample.size[findInterval(land.frame$age, c(7500, 8500, 9500, 10500)) %in% c(2, 4)], na.rm=TRUE)
 
-
 #  This isn't the best way to do it yet.
 self.long <- data.frame(uID  =rep(NA, self.size), 
                         aID  =rep(NA, self.size), 
                         diss =rep(NA, self.size), 
                         direction=rep(NA, self.size))
-
-land.long <- data.frame(uID  =rep(NA, land.size), 
-                        aID  =rep(NA, land.size), 
-                        diss =rep(NA, land.size), 
-                        direction=rep(NA, land.size))
 
 counter <- 1
 
@@ -88,69 +82,46 @@ for(i in 1:nrow(self.frame)){
 save(self.long, file = 'data/self.long.RData')
 
 #  Now this is for the landscape metric.  I expect it to be slower:
-if('land.frame.RData' %in% list.files('data')){
-  load('data/land.frame.RData')
-}
-else {
-  for(i in 1:nrow(land.frame)){
+load('data/land.frame.RData')
+
+land.long <- data.frame(uID  =rep(NA, land.size), 
+                        aID  =rep(NA, land.size), 
+                        diss =rep(NA, land.size), 
+                        direction=rep(NA, land.size))
+counter <- 1
+
+for(i in i:nrow(land.frame)){
+  if(findInterval(land.frame$age[i], c(7500, 8500, 9500, 10500)) %in% c(2, 4)){
     
-    #  The big analytic frame.
-    land.frame <- data.frame(site        = rep(compiled.pollen$sitename,2),
-                             dataset     = rep(compiled.pollen$dataset, 2),
-                             age         = rep(compiled.pollen$age, 2),
-                             index       = rep(1:nrow(compiled.pollen), 2),
-                             land.min    = rep(NA, nrow(compiled.pollen) * 2),
-                             land.pt     = rep(NA, nrow(compiled.pollen) * 2),
-                             sample.size = rep(NA, nrow(compiled.pollen) * 2),
-                             delta.age   = rep(NA, nrow(compiled.pollen)*2),
-                             mean.delt   = rep(NA, nrow(compiled.pollen)*2),
-                             direction   = rep(c('past', 'futu'), nrow(compiled.pollen)))
+    right.site <- compiled.pollen$sitename == land.frame$site[i]
+    site.ages <-  compiled.pollen$age[right.site]
+    sample.age <- (compiled.pollen$age == land.frame$age[i])
     
-      #  For each sample in the dataset we need to find it, and then check if it
-      #  has any samples that are between 250 and 750 years older than it.
-      right.site <- compiled.pollen$sitename == land.frame$site[i]
-      site.ages <-  compiled.pollen$age[right.site]
+    mult <- ifelse(land.frame$direction[i] == 'past',  1, -1)
+    
+    right.age <- findInterval(compiled.pollen$age,
+                              sort(c(land.frame$age[i] + 250 * mult,
+                                     land.frame$age[i] + 750 * mult))) == 1
+    
+    if(sum(right.age & right.site) > 1 & rep(no.others, 2)[i] == FALSE){
+      arrow <- cp.pct[land.frame$index[i],]
+      arrow.mat <- as.matrix(arrow)
       
-      mult <- ifelse(land.frame$direction[i] == 'past',  1, -1)
+      calib.samples <- cp.pct[right.age & (!right.site),]
+      calib.minus <- apply(calib.samples, 1, function(x) (arrow.mat - x)^2)
+      calib.vals <- sqrt(colSums(calib.minus, na.rm=TRUE))
       
-      right.age <- findInterval(compiled.pollen$age,
-                                sort(c(land.frame$age[i] + 250 * mult,
-                                       land.frame$age[i] + 750 * mult))) == 1
+      test <- data.frame(uID  = compiled.pollen$uID[floor((i+1)/2)],
+                         aID  = compiled.pollen$uID[(!right.site) & right.age],
+                         diss = calib.vals,
+                         direction = as.character(self.frame$direction[i]),
+                         stringsAsFactors = FALSE)
       
-      if(any((right.age & right.site) & sum(right.age) > 5)){
-        
-        arrow <- cp.pct[land.frame$index[i],]
-        arrow.mat <- as.matrix(arrow)
-        
-        calib.samples <- cp.pct[right.age & (!right.site), ]
-        
-        calib.sites <- compiled.pollen[right.age & (!right.site), 1]
-        
-        calib.minus <- apply(calib.samples, 1, function(x) (arrow.mat - x)^2)
-        
-        calib.vals <- sqrt(colSums(calib.minus, na.rm=TRUE))
-        
-        landscape.samples <- cp.pct[right.age & (!right.site), ]
-        
-        landscape.minus <- apply(landscape.samples, 1, function(x) (arrow.mat - x)^2)
-        landscape.vals  <- sqrt(colSums(landscape.minus, na.rm=TRUE))
-        
-        land.frame$land.pt[i] <- names(which.min(landscape.vals))
-        land.frame$sample.size[i] <- length(landscape.vals)
-        land.frame$land.min[i] <- min(landscape.vals)
-        land.frame$delta.age[i] <- compiled.pollen[land.frame$land.pt[i], ]$age - compiled.pollen$age[i]
-        land.frame$mean.delt[i] <- mean(compiled.pollen[names(landscape.vals), ]$age - compiled.pollen$age[i])
-      }
-        
-      #  Now we know that the sample has something to compare to (that is 
-      #  between 250 and 750 years older), we can create a vector for the sample.
-      #  We exclude 'Other', because it's super big some times.
-      
-      cat(paste(as.character(land.frame[i, 1]), 
-                        land.frame[i,2], 
-                        round(i/nrow(land.frame), 4)*100, sep=', '), '\n')
+      land.long[counter:(counter + length(calib.vals) - 1),] <- test
+      counter <- counter + length(calib.vals)
     }
   }
-  
-  save(land.frame, file='data/land.frame.RData')
+  cat(i, '\n')
 }
+
+save(land.long, file='data/land.long.RData')
