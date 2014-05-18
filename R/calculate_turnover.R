1455db1f11be08c1aa9116b8972868c1c3d2a659
Simon <simon.j.goring@gmail.com>
2014-05-18 16:18:09 -0500

    Finished parallel runs, re-wrote the turnover function so that it tests whether or not the file exists before running.

--- R/calculate_turnover.R
+++ R/calculate_turnover.R
@@ -11,131 +11,53 @@ sfInit(parallel = TRUE, cpus = 4)
 #  to exclude the sample.  This excludes fully a quarter of the sites in the dataset!
 #  n = 5441 of 21397
 
-pol.types <- c('Other', unique(as.character(pollen.equiv$WhitmoreSmall)))
-pol.types <- colnames(compiled.pollen)[colnames(compiled.pollen)%in%pol.types[!is.na(pol.types)]]
+source('analogue_function.R')
 
-cp.pct <- compiled.pollen[,pol.types]/rowSums(compiled.pollen[,pol.types])
-
-no.others <- cp.pct$Other > 0.10
-
-#  There are a set of parameters we're interested in:
-#  Minimum distances to the past & future for the landscape
-#  Spatial locations & time difference of the same.
-#  Minimum distances to the past & future for the site 
-#  Time to sample.
-
-compiled.pollen$uID <- 1:nrow(compiled.pollen)
-
-#  I'm breaking this into two so that we split the 'self' analysis away from the
-#  landscape analysis.  I keep it in a for loop because it's really long and
-#  the lapply functions require a lot of memory to keep everything together before
-#  dumping it out.
-
-if('self.frame.RData' %in% list.files('data')){
-  load('data/self.frame.RData')
+if('pollen.frame' %in% list.files('data/output/')){
+  load('data/output/pollen.frame.RData')
 }
-else {
-  self.frame <- data.frame(site      = rep(compiled.pollen$sitename,2),
-                           dataset   = rep(compiled.pollen$dataset, 2),
-                           age       = rep(compiled.pollen$age, 2),
-                           index     = rep(1:nrow(compiled.pollen), 2),
-                           self.min  = rep(NA, nrow(compiled.pollen)*2),
-                           self.size = rep(NA, nrow(compiled.pollen)*2),
-                           delta.age = rep(NA, nrow(compiled.pollen)*2),
-                           mean.delt = rep(NA, nrow(compiled.pollen)*2),
-                           direction = rep(c('past', 'futu'), nrow(compiled.pollen)))
+if(!'pollen.frame' %in% list.files('data/output/')){
+  #  Sets the empty dataframe for use in the ddply statement.  set.frame is declared in 'analogue_function.R'.
+  pollen.frame <- set.frame(compiled.pollen)
   
-  for(i in 1:nrow(self.frame)){
-    right.site <- compiled.pollen$sitename == self.frame$site[i]
-    site.ages <-  compiled.pollen$age[right.site]
-    mult <- ifelse(self.frame$direction[i] == 'past',  1, -1)
-    
-    right.age <- findInterval(compiled.pollen$age,
-                              sort(c(self.frame$age[i] + 250 * mult,
-                                     self.frame$age[i] + 750 * mult))) == 1
+  poll.in.compiled <- colnames(compiled.pollen) %in% c('Other', as.character(pollen.equiv$WhitmoreSmall))
   
-    if(sum(right.age & right.site) > 1 & rep(no.others, 2)[i] == FALSE){
-      arrow <- cp.pct[self.frame$index[i],]
-      arrow.mat <- as.matrix(arrow)
-      
-      calib.samples <- cp.pct[right.age & right.site,]
-      calib.minus <- apply(calib.samples, 1, function(x) (arrow.mat - x)^2)
-      calib.vals <- sqrt(colSums(calib.minus, na.rm=TRUE))
-      
-      self.frame$self.min[i]  <- min(calib.vals)
-      self.frame$delta.age[i] <- compiled.pollen$age[right.age & right.site][which.min(calib.vals)] - compiled.pollen$age[i]
-      self.frame$self.size[i] <- length(calib.vals)
-      self.frame$mean.delt[i] <- mean(compiled.pollen$age[right.age & right.site] - compiled.pollen$age[i], na.rm=TRUE)
-    }
-  }
+  #  We use the percents here, straight up.
+  compiled.pollen[,poll.in.compiled] <- compiled.pollen[,poll.in.compiled] / rowSums(compiled.pollen[,poll.in.compiled])
+  no.others <- cp.pct$Other > 0.10
   
-  save(self.frame, file = 'data/self.frame.RData')
+  #  A simple way to stop these samples from being considered in analysis.  If 'Other' is greater than 10% than
+  #  we discard the sample.
+  
+  compiled.pollen$Other[no.others] <- NA
+  
+  
+  #  This fills the dataset produced as climate.frame or pollen.frame 
+  pollen.fill <- ddply(pollen.frame, .(uniqueID), .parallel=TRUE, 
+                       .fun = find.analogues,
+                       compiled.data = compiled.pollen,
+                       .progress = 'text')
+  
+  pollen.frame[,colnames(pollen.fill)] <- pollen.fill
+  
+  save(pollen.frame, file='data/output/pollen.frame.RData')
 }
 
-#  Now this is for the landscape metric.  I expect it to be slower:
-if('land.frame.RData' %in% list.files('data')){
-  load('data/land.frame.RData')
+if('climate.frame' %in% list.files('data/output/')){
+  load('data/output/climate.frame.RData')
 }
-else {
-  for(i in 1:nrow(land.frame)){
-    
-    #  The big analytic frame.
-    land.frame <- data.frame(site        = rep(compiled.pollen$sitename,2),
-                             dataset     = rep(compiled.pollen$dataset, 2),
-                             age         = rep(compiled.pollen$age, 2),
-                             index       = rep(1:nrow(compiled.pollen), 2),
-                             land.min    = rep(NA, nrow(compiled.pollen) * 2),
-                             land.pt     = rep(NA, nrow(compiled.pollen) * 2),
-                             sample.size = rep(NA, nrow(compiled.pollen) * 2),
-                             delta.age   = rep(NA, nrow(compiled.pollen)*2),
-                             mean.delt   = rep(NA, nrow(compiled.pollen)*2),
-                             direction   = rep(c('past', 'futu'), nrow(compiled.pollen)))
-    
-      #  For each sample in the dataset we need to find it, and then check if it
-      #  has any samples that are between 250 and 750 years older than it.
-      right.site <- compiled.pollen$sitename == land.frame$site[i]
-      site.ages <-  compiled.pollen$age[right.site]
-      
-      mult <- ifelse(land.frame$direction[i] == 'past',  1, -1)
-      
-      right.age <- findInterval(compiled.pollen$age,
-                                sort(c(land.frame$age[i] + 250 * mult,
-                                       land.frame$age[i] + 750 * mult))) == 1
-      
-      if(any((right.age & right.site) & sum(right.age) > 5)){
-        
-        arrow <- cp.pct[land.frame$index[i],]
-        arrow.mat <- as.matrix(arrow)
-        
-        calib.samples <- cp.pct[right.age & (!right.site), ]
-        
-        calib.sites <- compiled.pollen[right.age & (!right.site), 1]
-        
-        calib.minus <- apply(calib.samples, 1, function(x) (arrow.mat - x)^2)
-        
-        calib.vals <- sqrt(colSums(calib.minus, na.rm=TRUE))
-        
-        landscape.samples <- cp.pct[right.age & (!right.site), ]
-        
-        landscape.minus <- apply(landscape.samples, 1, function(x) (arrow.mat - x)^2)
-        landscape.vals  <- sqrt(colSums(landscape.minus, na.rm=TRUE))
-        
-        land.frame$land.pt[i] <- names(which.min(landscape.vals))
-        land.frame$sample.size[i] <- length(landscape.vals)
-        land.frame$land.min[i] <- min(landscape.vals)
-        land.frame$delta.age[i] <- compiled.pollen[land.frame$land.pt[i], ]$age - compiled.pollen$age[i]
-        land.frame$mean.delt[i] <- mean(compiled.pollen[names(landscape.vals), ]$age - compiled.pollen$age[i])
-      }
-        
-      #  Now we know that the sample has something to compare to (that is 
-      #  between 250 and 750 years older), we can create a vector for the sample.
-      #  We exclude 'Other', because it's super big some times.
-      
-      cat(paste(as.character(land.frame[i, 1]), 
-                        land.frame[i,2], 
-                        round(i/nrow(land.frame), 4)*100, sep=', '), '\n')
-    }
-  }
+if(!'climate.frame' %in% list.files('data/output/')){
+  #  Sets the empty dataframe for use in the ddply statement.  set.frame is declared in 'analogue_function.R'.
+  climate.frame <- set.frame(compiled.climate)
   
-  save(land.frame, file='data/land.frame.RData')
+  #  This fills the dataset produced as climate.frame or pollen.frame 
+  climate.fill <- ddply(climate.frame, .(uniqueID), .parallel=TRUE, 
+                        .fun = find.analogues,
+                        compiled.data = compiled.climate,
+                        .progress = 'text')
+  
+  climate.frame[,colnames(climate.fill)] <- climate.fill
+  
+  save(climate.frame, file='data/output/climate.frame.RData')
+
 }
